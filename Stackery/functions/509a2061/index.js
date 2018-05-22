e1babf3b6621967ae46bce8a878a21fbc048facf
Toby Fee <TobyFee@gmail.com>
2018-05-22 11:42:56 -0700

    switch to emojification

--- Stackery/functions/509a2061/index.js
+++ Stackery/functions/509a2061/index.js
@@ -1,5 +1,14 @@
+var emoji = require('node-emoji');
+var MarkdownIt = require('markdown-it'),
+  md = new MarkdownIt();
+
+const inputString = 'I _really_ :heart:  :coffee:!'
+
+
 module.exports = async message => {
   console.dir(message);
 
-  return {};
+  return {
+    emoji.emojify(md.render(inputString))
+  };
 }
