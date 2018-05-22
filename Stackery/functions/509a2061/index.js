439efefeb23a4aba805d800b147c4ebbf84127ed
Toby Fee <TobyFee@gmail.com>
2018-05-22 11:56:59 -0700

    bugfix

--- Stackery/functions/509a2061/index.js
+++ Stackery/functions/509a2061/index.js
@@ -3,12 +3,12 @@ var MarkdownIt = require('markdown-it'),
   md = new MarkdownIt();
 
 const inputString = 'I _really_ :heart:  :coffee:!'
-
+let outputString = emoji.emojify(md.render(inputString))
 
 module.exports = async message => {
   console.dir(message);
 
   return {
-    emoji.emojify(md.render(inputString))
+    "response": outputString
   };
 }
